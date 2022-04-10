686e386a862c23b117e2c001a15f719f4422d2cb
Donald <donaldnallen87@gmail.com>
2022-04-09 20:58:29 -0700

    calc hypotenus

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+import math
+import sys
 
 def main():
-    print("HelloWorld")
+    _, a, b = sys.argv
+    a = float(a)
+    b = float(b)
+    c = calc_hypotenus(a,b)
+    print(c)
+
+
+def calc_hypotenus(a: float, b: float) -> float:
+    c = math.sqrt((a**2)+ (b**2))
+    return c
+    
 
 if __name__ == "__main__":
     main()
