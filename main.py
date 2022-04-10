63728454064990505be5832765b2e14ede81cb45
Donald <donaldnallen87@gmail.com>
2022-04-10 10:09:42 -0700

    add argparse

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import math
 import sys
+import argparse
 
 def main():
-    _, a, b = sys.argv
-    a = float(a)
-    b = float(b)
-    c = calc_hypotenus(a,b)
-    print(c)
+
+    parser = argparse.ArgumentParser(description='Process some numbers.')
+    parser.add_argument('numbers', metavar='N', type=float, nargs=2,
+                    help='an number for the accumulator')
+    parser.add_argument('--hypotenus', dest='hypotenus', action='store_const',
+                    const=calc_hypotenus, default=calc_hypotenus,
+                    help='calculate hypotenus')
+
+    args = parser.parse_args()
+    print(args.hypotenus(args.numbers))
 
 
-def calc_hypotenus(a: float, b: float) -> float:
+
+def calc_hypotenus(float_lst) -> float:
+    a, b = float_lst
     c = math.sqrt((a**2)+ (b**2))
     return c
     
